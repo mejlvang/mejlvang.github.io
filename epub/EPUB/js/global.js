ed0527c302625e760459c4d18f3702dbb427ed44
Asbjørn Mejlvang <asme@itu.dk>
2019-04-24 23:14:20 +0200

    Revert "test af epub"
    This reverts commit 448a25db39904eb74fd1d7ec10e71c9f00f715a4.

--- epub/EPUB/js/global.js
+++
@@ -1,7 +0,0 @@
-/*** Place your javascript code below ***/ 
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/addons/p5.dom.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/addons/p5.sound.min.js"></script>
-    
-    
